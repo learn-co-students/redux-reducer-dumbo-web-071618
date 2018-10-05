49208b6102ed540ed06f1f96abe40f486d975e60
Gabriel-Batista <gabedbatista@gmail.com>
2018-10-05 09:24:12 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
 export function manageFriends(state, action){
+    switch(action.type) {
+        case "ADD_FRIEND":
+            return { ...state, friends: [...state.friends, action.friend] }
+        case "REMOVE_FRIEND":
+            // let remove_index = state.friends.map(friend => friend.id).indexOf(action.id)
+            return {...state, friends: state.friends.filter((friend) => friend.id !== action.id)}
+        default:
+            return state
+    }
 }
