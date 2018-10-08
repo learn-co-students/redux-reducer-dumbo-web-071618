dc07bbb86e121d4f7438c20197bcb89b07e8c7d4
Isiah VanBrackle <squarepheonix@gmail.com>
2018-10-08 17:02:56 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,19 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends: []}, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return {...state, friends: [...state.friends, action.friend] }
+      // in the code above i spread into state, i go into the key of friends specifically which is an array
+      // then i spread into the state of friends and i add a friend to that state.
+    case 'REMOVE_FRIEND':
+    const friendIndex = state.friends.findIndex(friend => friend.id === action.id)
+      return ({
+        ...state,
+        friends: [...state.friends.slice(0, friendIndex), ...state.friends.slice(friendIndex+1)]
+      })
+      // in the code above we're trying to remove a friend to do this i put an empty object in my ()
+      // i'll spread into state specifically the key of friends first i getting everything before the friend i don't
+      // want to include next i'll concatnate it with  everything after the friend i don't want to include 
+        default:
+      return state;
+  }
 }
